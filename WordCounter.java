d667c7e274f906adb54c578b1f07b4b3f16da950
Alex <112178448+Alx424@users.noreply.github.com>
2024-02-01 22:56:37 +0000

    first commit

---
+++ WordCounter.java
@@ -0,0 +1,15 @@
+public class WordCounter {
+    public static main(String[] args) {
+        String str = "bob played connect-4 with his younger brother bobby and his other friend named bob";
+        String word = "bob";
+
+        String[] words = str.split("\\s+");
+        int count = 0;
+        for (String w : words) {
+            if (w.equalsIgnoreCase(word)) {
+                count++;
+            }
+        }
+        System.out.println(count);
+    }
+}
